89a0734348849f5edc3e023851b8ba24e25a5b18
Cbstray <calebstraily01@gmail.com>
2024-10-11 17:30:32 -0500

    Database Import and API Creation

---
+++ app/dao/restdao.js
@@ -0,0 +1,22 @@
+//IMPORT OUR MYSQL "pool" variable connection
+const pool = require('../config/dbconfig');
+
+class RestDao
+{
+    constructor() 
+    {
+        this.pool = pool;
+    }
+
+    findAll(req, res)
+    {
+        pool.query('SELECT * from restaurants_records', (err, rows) => 
+        {
+            console.log(rows);
+
+            res.send(rows);
+        })
+    }
+}
+
+module.exports = RestDao;
---
+++ app/routes/router.js
@@ -0,0 +1,8 @@
+//Import express package
+const express = require('express');
+
+const router = express.Router();
+
+router.use('/restaurants', require('./api/restroutes'));
+
+module.exports = router;
